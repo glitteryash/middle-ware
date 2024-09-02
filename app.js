c1148ddfa78f1957a798f4c03c12f6d3cfb64c61
desk-059 <hacchobori-05@desk-059.local>
2024-09-02 18:00:40 +0900

    second commit

--- app.js
+++ app.js
@@ -7,20 +7,29 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use((req, res, next) => {
   console.log("Hi! This is a first middleware.");
+  next();
+});
+app.use((req, res, next) => {
+  console.log("Hi! This is a second middleware.");
+  next();
 });
 
-mongoose
-  .connect("mongodb://localhost:27017/test", {
-    useFindAndModify: false,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("Connected to mongodb.");
-  })
-  .catch((e) => {
-    console.log(e);
-  });
+// mongoose
+//   .connect("mongodb://localhost:27017/test", {
+//     useFindAndModify: false,
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => {
+//     console.log("Connected to mongodb.");
+//   })
+//   .catch((e) => {
+//     console.log(e);
+//   });
+
+app.get("/", (req, res) => {
+  res.send("Welcome to homepage.");
+});
 
 app.listen(3000, () => {
   console.log("Server running on port 3000.");
