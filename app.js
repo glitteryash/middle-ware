7b9d334043004453e8743d04a92bad25ac321ba9
desk-059 <hacchobori-05@desk-059.local>
2024-09-02 17:55:39 +0900

    Initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+app.set("view engine", "ejs");
+// middlewares
+app.use(express.static("public"));
+app.use((req, res, next) => {
+  console.log("Hi! This is a first middleware.");
+});
+
+mongoose
+  .connect("mongodb://localhost:27017/test", {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to mongodb.");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000.");
+});
