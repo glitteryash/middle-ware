77917a5bf17f571b0bc2e87b7d6aae7fa41bb41f
desk-059 <hacchobori-05@desk-059.local>
2024-09-08 17:11:05 +0900

    update

--- app.js
+++ app.js
@@ -10,27 +10,49 @@ app.use((req, res, next) => {
   next();
 });
 
-// mongoose
-//   .connect("mongodb://localhost:27017/test", {
-//     useFindAndModify: false,
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => {
-//     console.log("Connected to mongodb.");
-//   })
-//   .catch((e) => {
-//     console.log(e);
-//   });
+mongoose
+  .connect("mongodb://localhost:27017/test")
+  .then(() => {
+    console.log("Connected to mongodb.");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
 
-app.get("/", (req, res) => {
-  res.sends("Welcome to homepage.");
+const monkeySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 5,
+  },
+});
+
+const Monkey = mongoose.model("Monkey", monkeySchema);
+
+// 處理async需要搭配try－catch
+app.get("/", async (req, res, next) => {
+  try {
+    let data = await Monkey.findOneAndUpdate(
+      { name: "JJJJJ" },
+      { name: "KKKKK" },
+      { new: true, runValidators: true }
+    );
+    if (!data) {
+      return res.status(404).send("Data is not found.");
+    }
+    res.send("Data has been updated.");
+  } catch (e) {
+    next(e);
+  }
+});
+
+app.get("/*", (res, req) => {
+  res.status(404).send("Paga not found");
 });
 
 //Error handlers
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).send("Something is broken");
+  res.status(500).send("Something is broken! <br> We will fix it soon");
 });
 
 app.listen(3000, () => {
